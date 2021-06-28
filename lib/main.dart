fb6307eaf026f56211ac4b770d510e6c77f8a2dc
Zune Zune <zunezune@Zunes-MacBook-Pro.local>
2021-06-28 15:39:07 +0630

    animate list practice

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:animatedTodoList/data/list_item.dart';
-import 'package:animatedTodoList/model/list_item.dart';
 import 'package:animatedTodoList/widget/list_item_widget.dart';
 import 'package:flutter/material.dart';
+
+import 'model/list_item.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,29 +14,76 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: _MainPage(),
+      theme: ThemeData.dark(),
+      home: MyHomePage(title: 'Animated List'),
     );
   }
-
-  
 }
 
-class _MainPage extends StatelessWidget {
-  final List<ListItem> items=List.from(listItems);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
 
-    @override
-    Widget build(BuildContext context) {
-      return AnimatedList(
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /// Will used to access the Animated list
+  final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
+
+  final List<ListItem> items = List.from(listItems);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: AnimatedList(
+        key: listKey,
         initialItemCount: items.length,
         itemBuilder: (context, index, animation) => ListItemWidget(
           item: items[index],
           animation: animation,
-          onClicked: (){},
+          onClicked: () {
+            removeItem(index);
+          },
         ),
-      );
-    }
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: insertItem,
+      ),
+    );
   }
+
+  void insertItem() {
+    int newIndex = 1;
+    ListItem newItem = (List.of(listItems)..shuffle()).first;
+
+    items.insert(newIndex, newItem);
+    listKey.currentState.insertItem(newIndex,
+        duration: Duration(
+          milliseconds: 600,
+        ));
+  }
+
+  void removeItem(index) {
+    final removeItem = items[index];
+
+    items.removeAt(index);
+    listKey.currentState.removeItem(
+      index,
+      (context, animation) => ListItemWidget(
+        item: removeItem,
+        animation: animation,
+        onClicked: () {},
+      ),
+      duration: Duration(
+        milliseconds: 600,
+      ),
+    );
+  }
+}
--- lib/widget/list_item_widget.dart
+++ lib/widget/list_item_widget.dart
@@ -6,23 +6,55 @@ class ListItemWidget extends StatelessWidget {
   final Animation<double> animation;
   final VoidCallback onClicked;
 
-  const ListItemWidget({Key key,@required this.item, this.animation, this.onClicked}) : super(key: key);
+  const ListItemWidget({
+    Key key,
+    @required this.item,
+    @required this.animation,
+    @required this.onClicked,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return buildItem();
-
+    return SizeTransition(
+      key: ValueKey(item.urlImage),
+      sizeFactor: animation,
+      child: buildItem(),
+    );
   }
 
-  Widget buildItem() => Container(
-    margin: EdgeInsets.all(8),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(20),
-      color: Colors.white,
-    ),
-    child: ListTile(
-      contentPadding: EdgeInsets.all(12),
-    ),
-  );
+  // SlideTransition(
+  //     key: ValueKey(item.urlImage),
+  //     position: Tween<Offset>(
+  //       begin: const Offset(-1, 0),
+  //       end: Offset(0, 0),
+  //     ).animate(animation),
+  //     child: buildItem(),
+  //   );
 
+  Widget buildItem() => Container(
+        margin: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child: ListTile(
+          contentPadding: EdgeInsets.all(12),
+          leading: CircleAvatar(
+            radius: 32,
+            backgroundImage: NetworkImage(item.urlImage),
+          ),
+          title: Text(
+            item.title,
+            style: TextStyle(fontSize: 20, color: Colors.black),
+          ),
+          trailing: IconButton(
+            onPressed: onClicked,
+            icon: Icon(
+              Icons.delete,
+              color: Colors.red,
+              size: 30,
+            ),
+          ),
+        ),
+      );
 }
